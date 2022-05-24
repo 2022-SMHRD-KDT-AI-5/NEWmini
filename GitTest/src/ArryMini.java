903c3657a9d9ecfc0b2359fe355195b4f6afbd88
smhrd <smhrd@DESKTOP-B6C41VT>
2022-05-24 10:13:42 +0900

    adsf

--- GitTest/src/ArryMini.java
+++ GitTest/src/ArryMini.java
@@ -6,60 +6,83 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ArryMini {
-	
+
 	public static void main(String[] args) {
-		
-		ArrayList<Answer> list = new ArrayList<Answer>();
+
+		ArrayList<Answer> listhard = new ArrayList<Answer>();
+		ArrayList<Answer> listeasy = new ArrayList<Answer>();
+		ArrayList<Answer> listnomal = new ArrayList<Answer>();
 		PreparedStatement psmt = null;
 		Connection conn = null;
 		ResultSet rs = null;
-		
+
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			String url = "jdbc:oracle:thin:@project-db-stu.ddns.net:1524:xe";
 			String user = "cgi_7_0516_5";
 			String password = "smhrd5";
-			conn = DriverManager.getConnection(url,user,password);
+			conn = DriverManager.getConnection(url, user, password);
 			System.out.println("DB connect");
 		} catch (ClassNotFoundException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 		Minicontroller mc = new Minicontroller(conn);
 		int index = 1;
 		
 		try {
-			String sql = ("select * from hard");
-			psmt = conn.prepareStatement(sql);
+			String sqlhard = ("select * from hard");
+			psmt = conn.prepareStatement(sqlhard);
 			rs = psmt.executeQuery();
-			while(rs.next()) { //데이터베이스형식과 java 형식이 다름.
-				index = 1; 	//1부터 시작
+			while (rs.next()) {
+				index = 1;
 				String kor = rs.getString(index++);
 				String eng = rs.getString(index++);
-				list.add(new Answer(kor, eng));
+				listhard.add(new Answer(kor, eng));
 			}
-		} catch (SQLException e) {
+		}catch (SQLException e) {
 			e.printStackTrace();
-		} finally {
-				mc.close();
-			}
-		
-		
-		for(Answer model : list) {
-			System.out.println(model.getEng() +model.getKor());
 		}
-
-	
-
-
-
-
-
-
+		try {
+			String sqleasy = ("select * from easy");
+			psmt = conn.prepareStatement(sqleasy);
+			rs = psmt.executeQuery();
+			while (rs.next()) {
+				index = 1;
+				String kor = rs.getString(index++);
+				String eng = rs.getString(index++);
+				listeasy.add(new Answer(kor, eng));
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			String sqlnomal = ("select * from nomal");
+			psmt = conn.prepareStatement(sqlnomal);
+			rs = psmt.executeQuery();
+			while (rs.next()) {
+				index = 1;
+				String kor = rs.getString(index++);
+				String eng = rs.getString(index++);
+				listnomal.add(new Answer(kor, eng));
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			mc.close();
+		}
+		for (int i = 0; i < listhard.size(); i++) {
+			System.out.println(listhard.get(i).getKor() + listhard.get(i).getEng());
+		}
+		System.out.println("//");
+		for (int i = 0; i < listnomal.size(); i++) {
+			System.out.println(listnomal.get(i).getKor() + listnomal.get(i).getEng());
+		}
+		System.out.println("//");
+		for (int i = 0; i < listeasy.size(); i++) {
+			System.out.println(listeasy.get(i).getKor() + listeasy.get(i).getEng());
+		}
 	}
 
 }
